a0c7f26b0b517ed2f8d18c76ccf6e26ff343d5c2
lizhiqiu <lizhiqiu@baidu.com>
2025-01-15 18:36:47 +0800

    删除

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -12,49 +12,7 @@ export const routes = [
           keepAlive: true,
         },
       },
-      {
-        path: 'list',
-        component: () => import('@/views/list/index.vue'),
-        meta: {
-          title: 'tabbar.list',
-          keepAlive: true,
-        },
-      },
-      {
-        path: 'member',
-        component: () => import('@/views/member/index.vue'),
-        meta: {
-          title: 'tabbar.member',
-          keepAlive: true,
-        },
-      },
-      {
-        path: 'demo',
-        component: () => import('@/views/demo/index.vue'),
-        meta: {
-          title: 'tabbar.demo',
-          keepAlive: true,
-        },
-      },
-      {
-        name: 'listDetails',
-        path: '/details',
-        component: () => import('@/views/list/details/index.vue'),
-        meta: {
-          title: 'list.details',
-          border: false,
-        },
-      },
     ],
-  },
-  {
-    name: 'login',
-    path: '/login',
-    component: () => import('@/views/login/index.vue'),
-    meta: {
-      title: '',
-      keepAlive: true,
-    },
   },
   // 匹配不到重定向会主页
   {

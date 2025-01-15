eb141dbc6342215163befbc05ede388761934357
lizhiqiu <lizhiqiu@baidu.com>
2025-01-15 17:40:17 +0800

    测试

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { i18n } from '@/i18n';
+import router from '@/router';
+import store from '@/store';
+import './assets/font/iconfont.css';
+import './assets/app.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// 路由
+app.use(router);
+
+// 国际化
+app.use(i18n);
+
+// 状态管理
+app.use(store);
+
+app.mount('#app');
